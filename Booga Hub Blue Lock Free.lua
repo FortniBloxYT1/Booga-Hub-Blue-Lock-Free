14205df59c719ee8706754747900a6bf1d53cf5b
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2025-06-20 17:23:21 +0200

    Update Booga Hub Blue Lock Free.lua

--- Booga Hub Blue Lock Free.lua
+++ Booga Hub Blue Lock Free.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c57e85bed2a0be5a16dfd09ea891face.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34df016c3818fcaf2f91eb7a0934849c.lua"))()
